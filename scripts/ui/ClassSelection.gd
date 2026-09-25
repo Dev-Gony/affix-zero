8d786a08bd3d62ebbab38fd21a23bccaccf364f8
Dev-Gony <gwonyi190000@gmail.com>
2026-09-25 18:42:05 +0900

    ui: switch class selection cards to generated hero atlas

--- scripts/ui/ClassSelection.gd
+++ scripts/ui/ClassSelection.gd
@@ -11,13 +11,10 @@ const CLASS_PATHS: Array[String] = [
 	"res://resources/classes/assassin.tres",
 	"res://resources/classes/saint.tres",
 ]
-const CLASS_TEXTURES := {
-	"warrior": preload("res://assets/cc0/tiny_dungeon/warrior.png"),
-	"mage": preload("res://assets/cc0/tiny_dungeon/mage.png"),
-	"knight": preload("res://assets/cc0/tiny_dungeon/knight.png"),
-	"sage": preload("res://assets/cc0/tiny_dungeon/sage.png"),
-	"assassin": preload("res://assets/cc0/tiny_dungeon/assassin.png"),
-	"saint": preload("res://assets/cc0/tiny_dungeon/saint.png"),
+const CLASS_ATLAS: Texture2D = preload("res://assets/sprites/class_atlas_alpha.png")
+const CLASS_REGIONS: Dictionary = {
+	"warrior": Vector2i(0, 0), "mage": Vector2i(1, 0), "knight": Vector2i(2, 0),
+	"sage": Vector2i(0, 1), "assassin": Vector2i(1, 1), "saint": Vector2i(2, 1),
 }
 
 var _grid: GridContainer
@@ -138,7 +135,12 @@ func _card_style(background: Color, border: Color, width: int) -> StyleBoxFlat:
 
 
 func _make_class_icon(class_data: ClassData) -> Texture2D:
-	return CLASS_TEXTURES.get(class_data.id, CLASS_TEXTURES["warrior"])
+	var atlas_cell: Vector2i = CLASS_REGIONS.get(class_data.id, Vector2i.ZERO)
+	var cell_size := Vector2(float(CLASS_ATLAS.get_width()) / 3.0, float(CLASS_ATLAS.get_height()) / 2.0)
+	var icon := AtlasTexture.new()
+	icon.atlas = CLASS_ATLAS
+	icon.region = Rect2(Vector2(atlas_cell) * cell_size, cell_size)
+	return icon
 
 
 func _select_class(class_data: ClassData) -> void:
